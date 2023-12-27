99afcbb2e5eb149394499bcdf2623f0798d81b3b
julian <julian.bottiglione@epitech.eu>
2023-12-27 10:44:22 +0100

    [save] redumping pc

--- whale/build_on_docker.sh
+++ whale/build_on_docker.sh
@@ -5,7 +5,6 @@ sudo docker image build -t jenkins-image .
 sudo docker container run \
     --name my-jenkins \
     --restart=on-failure \
-    --detach \
     --network jenkins-network \
     --env DOCKER_HOST=tcp://docker:2376 \
     --env DOCKER_CERT_PATH=/certs/client \
@@ -14,7 +13,8 @@ sudo docker container run \
     --publish 50000:50000 \
     --volume jenkins-data:/var/jenkins_home \
     --volume jenkins-docker-certs:/certs/client:ro \
-    jenkins-image
+    jenkins-image 
+    #--detach 
     #--volume /var/run/docker.sock:/var/run/docker.sock
 
 #the last volume has been added to allow DinD
